332bcaac460360fbee3ca840ecfcce85ed983404
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-03-20 16:48:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ var balloon,balloonImage1,balloonImage2;
 var height;
 
 function preload(){
-   bg =loadImage("Images/cityImage.png");
+//load background image
+   
+   
    balloonImage1=loadAnimation("Images/HotAirBallon-01.png");
    balloonImage2=loadAnimation("Images/HotAirBallon-01.png","Images/HotAirBallon-01.png",
    "Images/HotAirBallon-01.png","Images/HotAirBallon-02.png","Images/HotAirBallon-02.png",
@@ -21,33 +23,20 @@ function setup() {
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
-  var balloonHeight=database.ref('balloon/height');
-  balloonHeight.on("value",readHeight, showError);
-  textSize(20); 
+  //take ref from database for balloon height
+   
+   
 }
 
 
 function draw() {
   background(bg);
 
-  if(keyDown(LEFT_ARROW)){
-    updateHeight(-10,0);
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-  }
-  else if(keyDown(RIGHT_ARROW)){
-    updateHeight(10,0);
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-  }
-  else if(keyDown(UP_ARROW)){
-    updateHeight(0,-10);
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-    balloon.scale=balloon.scale -0.005;
-  }
-  else if(keyDown(DOWN_ARROW)){
-    updateHeight(0,+10);
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
-    balloon.scale=balloon.scale+0.005;
-  }
+ //add arrow keys and update height positions
+   
+   
+   
+   
 
   drawSprites();
   fill(0);
